5d369fd5eada82c82f4cbc89a2d2c1f0544269b9
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2021-12-28 15:35:09 -0300

    First component

---
+++ src/components/basics/First.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function First(){
+  const msg = 'Meu primeiro app com React.'
+  return (
+    <div>
+      <h2>Primeiro Componente</h2>
+      <p>{ msg }</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+import './index.css';
 import ReactDom from 'react-dom';
 import React from 'react';
 
-import './index.css';
+import First from './components/basics/First';
+
+
+const el = document.getElementById('root');
+
+ReactDom.render(
+  <div>
+    <First></First>
+  </div>, 
+
+el); 
 
 /*
 const el = document.getElementById('root');
@@ -10,12 +21,3 @@ ReactDom.render(
   // sintaxe JSX, parece html, mas não é. O JSX fará uma conversão para mostrar na página como se fosse HTML via JS. Para funcionar isso precisa do import React.
   el);
   // nesse código se passa dois parâmetros: o que quer que seja passado, e onde passar. o document.getElement ... poderia ser criado diretamente no segundo parâmetro no lugar da constante (que poderia ser uma variável). */
-
-const el = document.getElementById('root');
-const tag = <strong>Olá mundo!</strong>;
-ReactDom.render(
-  <div>
-    { tag }
-  </div>, 
-
-el); 
