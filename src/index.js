cc15c3e8e046efc0fea2696919ac33e63dbf3e09
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2021-12-28 15:07:51 -0300

    Introducing JSX in project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import ReactDom from 'react-dom';
+import React from 'react';
+
+/*
+const el = document.getElementById('root');
+ReactDom.render(
+  <div><strong>Olá mundo!</strong></div>, 
+  // sintaxe JSX, parece html, mas não é. O JSX fará uma conversão para mostrar na página como se fosse HTML via JS. Para funcionar isso precisa do import React.
+  el);
+  // nesse código se passa dois parâmetros: o que quer que seja passado, e onde passar. o document.getElement ... poderia ser criado diretamente no segundo parâmetro no lugar da constante (que poderia ser uma variável). */
 
 const el = document.getElementById('root');
-ReactDom.render('Olá mundo!', el);
+const tag = <strong>Olá mundo!</strong>;
+ReactDom.render(
+  <div>
+    { tag }
+  </div>, 
+  
+el); 
